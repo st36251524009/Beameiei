b5b4b4a853a1cd1e27b1833c87363eb90a61003d
st36251524009 <52314794+st36251524009@users.noreply.github.com>
2019-07-22 09:59:12 +0700

    Add files via upload

---
+++ HW2_1.png.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+#นาย ปรเมษฐ์  ไพโรจน์  362515241009 EE36241
+print("-----------Menu-----------")
+print("Welcome to Icecream Shop")
+print("1. Strawberry    80 THB")
+print("2. Vanila           50 THB")
+print("3. Chocolate     40 THB")
+print("4. Lemon         90 THB")
+print("5. Milk             10 THB")
+icecream  =   int(input("What do you want?"))
+if   icecream==  1 :
+    print("You order : 1 Strawberry 80 THB")
+elif icecream==2 :
+    print("You order : 2 Vanila 50 THB")
+elif icecream==3 :
+    print("You order : 3 Chocolate 40 THB")
+elif icecream==4 :
+    print("You order : 4 Lemon 90 THB")
+elif icecream==5 :
+    print("You order : 5 Milk 10 THB")
+
+
+# In[ ]:
+
+
+
+
